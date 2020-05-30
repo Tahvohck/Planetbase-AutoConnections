2b923ae09770e99c56b5a657636ec001632dbb0a
Tahvohck <Tahvohck@gmail.com>
2020-05-29 22:50:51 -0400

    Add state holder for code readability

--- AutoConnect_Main.cs
+++ AutoConnect_Main.cs
@@ -32,41 +32,53 @@ namespace Tahvohck_Mods.JPFariasUpdates
     [HarmonyPatch(typeof(GameStateGame), "placeModule")]
     public class PatchGameState
     {
-        public static void Prefix(GameStateGame __instance, out object[] __state)
+        public static void Prefix(GameStateGame __instance, out State __state)
         {
             if (__instance.mActiveModule.isValidPosition()) {
-                __state = new object[2];
-                __state[0] = __instance.mActiveModule;
-                __state[1] = __instance.mRenderTops;
+                __state = new State(__instance.mActiveModule, __instance.mRenderTops);
             } else {
                 __state = null;
             }
         }
 
-        public static void Postfix(object[] __state)
+        public static void Postfix(State __state)
         {
             // Don't run if module position was invalid
             if (__state is null) { return; }
 
-            Module currentModule = __state[0] as Module;
-            bool renderTops = (bool)__state[1];
-            TahvUtil.Log($"Running connections on a {currentModule.getName()}");
+            TahvUtil.Log($"Running connections on a {__state.ActiveModule.getName()}");
 
             List<Module> linkable = Module.mModules.FindAll((module) =>
                 !(module is null)
-                && module != currentModule
+                && module != __state.ActiveModule
                 && Connection.canLink(
-                    currentModule, module,
-                    currentModule.getPosition(), module.getPosition())
+                    __state.ActiveModule, module,
+                    __state.ActiveModule.getPosition(), module.getPosition())
             );
 
             linkable.ForEach(delegate (Module module) {
-                Connection c = Connection.create(currentModule, module);
+                Connection c = Connection.create(__state.ActiveModule, module);
                 c.onUserPlaced();
-                c.setRenderTop(renderTops);
-                currentModule.recycleLinkComponents();
+                c.setRenderTop(__state.RenderTops);
+                __state.ActiveModule.recycleLinkComponents();
                 module.recycleLinkComponents();
             });
+        }
+
+
+        /// <summary>
+        /// Internal state holder
+        /// </summary>
+        public class State
+        {
+            public readonly Module ActiveModule;
+            public readonly bool RenderTops;
+
+            public State(Module m, bool renderTops)
+            {
+                ActiveModule = m;
+                RenderTops = renderTops;
+            }
         }
     }
 }
