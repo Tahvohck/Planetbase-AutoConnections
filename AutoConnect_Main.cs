6c4c1aa9a0589b94bb5b5deb21c2f1f838586c19
Tahvohck <Tahvohck@gmail.com>
2020-05-29 22:33:34 -0400

    Initial, fully working version.

---
+++ AutoConnect_Main.cs
@@ -0,0 +1,68 @@
+﻿using HarmonyLib;
+using Planetbase;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Tahvohck_Mods.JPFariasUpdates
+{
+    public class Mod : IMod, ITahvUtilMod
+    {
+        public void Init()
+        {
+            ZZZ_Modhooker.UtilsReadyEvent += Setup;
+        }
+
+        public void Update()
+        { }
+
+        public void Setup(object caller, EventArgs evArgs)
+        {
+            new Harmony(typeof(Mod).FullName).PatchAll();
+            TahvUtil.Log("Patched.");
+        }
+    }
+
+
+    [HarmonyPatch(typeof(GameStateGame), "placeModule")]
+    public class PatchGameState
+    {
+        public static void Prefix(GameStateGame __instance, out object[] __state)
+        {
+            if (__instance.mActiveModule.isValidPosition()) {
+                __state = new object[2];
+                __state[0] = __instance.mActiveModule;
+                __state[1] = __instance.mRenderTops;
+            } else {
+                __state = null;
+            }
+        }
+
+        public static void Postfix(object[] __state)
+        {
+            // Don't run if instance is null
+            if (__state is null) { return; }
+
+            Module currentModule = __state[0] as Module;
+            bool renderTops = (bool)__state[1];
+            TahvUtil.Log($"Running connections on a {currentModule.getName()}");
+
+            List<Module> linkable = Module.mModules.FindAll((module) =>
+                !(module is null)
+                && module != currentModule
+                && Connection.canLink(
+                    currentModule, module,
+                    currentModule.getPosition(), module.getPosition())
+            );
+
+            linkable.ForEach(delegate (Module module) {
+                Connection c = Connection.create(currentModule, module);
+                c.onUserPlaced();
+                c.setRenderTop(renderTops);
+                currentModule.recycleLinkComponents();
+                module.recycleLinkComponents();
+            });
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AutoConnections")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AutoConnections")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d00e2232-c17c-44d9-ab0a-14aa331e5ac6")]
